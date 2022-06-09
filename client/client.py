44de030c8dc1610be717d2ee2258908c9ce90dac
Ryu0118 <ryunosuke.shibuya.3d@stu.hosei.ac.jp>
2022-06-10 03:04:35 +0900

    fixed a bug

--- client/client.py
+++ client/client.py
@@ -3,9 +3,14 @@ import websockets
 import pyautogui
 
 
+async def receive(websocket):
+    char = await websocket.recv()
+    pyautogui.press(char)
+    await receive(websocket)
+
+
 async def main():
     url = "ws://localhost:8765"
     async with websockets.connect(url) as websocket:
         print(f'Successfully connect {url}')
-        char = await websocket.recv()
-        pyautogui.press(char)
+        await receive(websocket)
