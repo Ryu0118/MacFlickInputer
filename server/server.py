36494bce9bbe82b7799ffd585c1826c66c7fca9b
Ryu0118 <ryunosuke.shibuya.3d@stu.hosei.ac.jp>
2022-06-10 17:43:07 +0900

    added link and delete handle

--- server/server.py
+++ server/server.py
@@ -1,13 +1,26 @@
 import asyncio
+import time
+import pyautogui
+import pyperclip
 import websockets
 
 
 async def echo(websocket, path):
     async for message in websocket:
-        print(message)
-        await websocket.send(message)
+        print("receive message:", message)
+        data = message.split(':')
+        if len(data) == 2 and data[0] == 'client':
+            if data[1] == 'link':
+                pyautogui.hotkey('command', 'shift', 'left')
+                pyautogui.hotkey('command', 'c')
+                websocket.send('server:' + pyperclip.paste())
+            elif data[1] == 'delete':
+                pyautogui.hotkey('delete')
+        else:
+            pyperclip.copy(message)
+            pyautogui.hotkey('command', 'v')
 
 
 async def main():
-    async with websockets.serve(echo, "localhost", 8765):
+    async with websockets.serve(echo, "192.168.0.56", 8765):
         await asyncio.Future()
